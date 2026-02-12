586c9500f09633757509cd63694d107809080966
Glenn Gillen <me@glenngillen.com>
2026-02-12 23:51:05 +1100

    Update bin file to instantiate SQL Server Bahrain stack

--- bin/demo-cdk.ts
+++ bin/demo-cdk.ts
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib/core';
-import { DemoCdkStack } from '../lib/demo-cdk-stack';
+import { DemoCdkStack, SqlServerBahrainStack } from '../lib/demo-cdk-stack';
 
 const app = new cdk.App();
+
+// Main demo stack (environment-agnostic)
 new DemoCdkStack(app, 'DemoCdkStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
@@ -17,4 +19,12 @@ new DemoCdkStack(app, 'DemoCdkStack', {
   // env: { account: '123456789012', region: 'us-east-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+});
+
+// SQL Server Enterprise Multi-AZ stack in Bahrain region (me-south-1)
+new SqlServerBahrainStack(app, 'SqlServerBahrainStack', {
+  env: {
+    region: 'me-south-1', // Bahrain region
+  },
+  description: 'SQL Server Enterprise Multi-AZ (db.r5.24xlarge) in Bahrain region',
 });
