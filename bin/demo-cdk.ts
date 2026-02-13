a1e07d0a3a3a9620f3a8eb89324a8769a7562fc8
Glenn Gillen <me@glenngillen.com>
2026-02-13 21:47:01 +1100

    Add region so prices are calculated

--- bin/demo-cdk.ts
+++ bin/demo-cdk.ts
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib/core';
-import { DemoCdkStack, SqlServerBahrainStack } from '../lib/demo-cdk-stack';
+import * as cdk from "aws-cdk-lib/core";
+import { DemoCdkStack, SqlServerBahrainStack } from "../lib/demo-cdk-stack";
 
 const app = new cdk.App();
 
 // Main demo stack (environment-agnostic)
-new DemoCdkStack(app, 'DemoCdkStack', {
+new DemoCdkStack(app, "DemoCdkStack", {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -19,12 +19,16 @@ new DemoCdkStack(app, 'DemoCdkStack', {
   // env: { account: '123456789012', region: 'us-east-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+  env: {
+    region: "us-east-1", // Virginia region
+  },
 });
 
 // SQL Server Enterprise Multi-AZ stack in Bahrain region (me-south-1)
-new SqlServerBahrainStack(app, 'SqlServerBahrainStack', {
+new SqlServerBahrainStack(app, "SqlServerBahrainStack", {
   env: {
-    region: 'me-south-1', // Bahrain region
+    region: "me-south-1", // Bahrain region
   },
-  description: 'SQL Server Enterprise Multi-AZ (db.r5.24xlarge) in Bahrain region',
+  description:
+    "SQL Server Enterprise Multi-AZ (db.r5.24xlarge) in Bahrain region",
 });
